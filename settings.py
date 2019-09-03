c531084e299247beb45af2a592398aea9d6f06fd
imyhui <1359158019@qq.com>
2019-09-03 10:36:27 +0800

    feat: add settings

---
+++ settings.py
@@ -0,0 +1,9 @@
+# 相关配置
+
+
+#------------database----------------
+HOST = "localhost"
+USER = "root"
+PASSWD = "123456"
+PORT = 3306
+DB = "doubans"
--- store.py
+++ store.py
@@ -1,12 +1,14 @@
 import pymysql
+import settings
 
 
 def info_save(data):
-    db = pymysql.connect(host="localhost", user="root", passwd="123456", port=3306, db="douban", charset="utf8")
+    db = pymysql.connect(host=settings.HOST, user=settings.USER, passwd=settings.PASSWD, port=3306, db=settings.DB,
+                         charset="utf8")
     douban_id, title, cover, score, year, summary, director, screenwriter, mainactors, tags, countries, languages, release_time, length, imdb_url, othername, evaluation_nums, shortcom_nums, comment_nums = data
     cur = db.cursor()
     sql_movie = f"""
-    insert into `douban`.`movies` (
+    insert into `movies` (
                 `douban_id`,
                 `title`,
                 `cover`,
@@ -38,21 +40,21 @@ def info_save(data):
             add_tags = []
             for tag in tags:
                 add_tags.append((douban_id, pymysql.escape_string(tag)))
-            cur.executemany('insert into `douban`.`tags` (`movie_id`,`tag_name`) value(%s,%s)',
+            cur.executemany('insert into `tags` (`movie_id`,`tag_name`) value(%s,%s)',
                             add_tags)
         if screenwriter != "":
             add_wrs = []
             screenwriter = screenwriter.split("/")
             for sn_writer in screenwriter:
                 add_wrs.append((douban_id, pymysql.escape_string(sn_writer)))
-            cur.executemany('insert into `douban`.`screenwriters` (`movie_id`,`screenwriter_name`) value(%s,%s)',
+            cur.executemany('insert into `screenwriters` (`movie_id`,`screenwriter_name`) value(%s,%s)',
                             add_wrs)
         if mainactors != "":
             add_macs = []
             mainactors = mainactors.split("/")
             for main_ac in mainactors:
                 add_macs.append((douban_id, pymysql.escape_string(main_ac)))
-            cur.executemany('insert into `douban`.`mainactors` (`movie_id`,`mainactor_name`) value(%s,%s)',
+            cur.executemany('insert into `mainactors` (`movie_id`,`mainactor_name`) value(%s,%s)',
                             add_macs)
         db.commit()
         print(f"电影 {title} 插入成功")
